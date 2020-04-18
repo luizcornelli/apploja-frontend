dadda4e469d51fef16227d55ca2922baa35c4634
Luiz Cornelli <luiz.cavalcanti@unidac.com.br>
2020-04-18 03:10:57 -0300

    Mostrando as Categorias

--- src/pages/categorias/categorias.ts
+++ src/pages/categorias/categorias.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import { CategoriaService } from '../../services/domain/categoria.service';
+import { CategoriaDTO } from '../../models/categoria.dto';
+import { API_CONFIG } from '../../config/api.config';
 
 @IonicPage()
 @Component({
@@ -8,6 +10,9 @@ import { CategoriaService } from '../../services/domain/categoria.service';
   templateUrl: 'categorias.html',
 })
 export class CategoriasPage {
+
+  bucketBaseUrl: string = API_CONFIG.bucketBaseUrl;
+  itens: CategoriaDTO[];
 
   constructor(public navCtrl: NavController, 
     public navParams: NavParams, 
@@ -19,7 +24,7 @@ export class CategoriasPage {
     this.categoriaService.findAll()
       .subscribe(response => {
 
-        console.log(response); 
+        this.itens = response; 
       }, 
       erro => {
         console.log(erro);
