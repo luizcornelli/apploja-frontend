b73a5c6fdd65f359ed299acf85fac042b66d3eb9
Luiz Cornelli <luiz.cavalcanti@unidac.com.br>
2020-04-18 13:14:08 -0300

    Salvando dados do usuário logado no localStorage do html5

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -26,7 +26,8 @@ export class HomePage {
     
     this.auth.authenticate(this.credenciais)
       .subscribe(response => {
-        console.log(response.headers.get('Authorization'));
+        
+        this.auth.successfulLogin(response.headers.get('Authorization'))        
         this.navCtrl.setRoot('CategoriasPage');
       },
       erro =>{});
