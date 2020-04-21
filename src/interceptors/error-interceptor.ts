4af6b0ff8af6d3e1e0608db49883e2d9f4845497
Luiz Cornelli <luiz.cavalcanti@unidac.com.br>
2020-04-21 01:42:05 -0300

    Tratamento padrão para outros erros com Alert

--- src/interceptors/error-interceptor.ts
+++ src/interceptors/error-interceptor.ts
@@ -2,11 +2,13 @@ import { Injectable } from '@angular/core';
 import { HttpEvent, HttpInterceptor, HttpHandler, HttpRequest, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { Observable } from 'rxjs/Rx'
 import { StorageService } from '../services/storage.service';
+import { AlertController } from 'ionic-angular';
 
 @Injectable()
 export class ErrorInterceptor implements HttpInterceptor {
 
-    constructor(public storage: StorageService){
+    constructor(public storage: StorageService, 
+        public alertController: AlertController){
     }
 
     intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
@@ -26,13 +28,54 @@ export class ErrorInterceptor implements HttpInterceptor {
 
                 switch(errorObj.status){
                     
+                    case 401: 
+                    this.handle401(); 
+                    break;
+
                     case 403: 
                     this.handle403(); 
                     break;
+
+                    default: 
+                    this.handleDefaultError(errorObj);
                 }
 
                 return Observable.throw(errorObj);
             }) as any;
+    }
+
+    handleDefaultError(errorObj) {
+
+        let alert = this.alertController.create({
+
+            title: 'Erro ' + errorObj.status + ': ' + errorObj.error,
+            message: errorObj.message, 
+            enableBackdropDismiss: false, 
+            buttons: [
+                {
+                    text: 'Ok'
+                }
+            ]
+        });
+
+        alert.present(); 
+    }
+
+    handle401() {
+
+        let alert = this.alertController.create({
+
+            title: 'Erro 401: falha de autenticação', 
+            message: 'Email ou senha incorretos', 
+            enableBackdropDismiss: false, 
+            buttons: [
+                {
+                    text: 'Ok'
+                }
+            ]
+        });
+
+        alert.present(); 
     }
 
     handle403() {
