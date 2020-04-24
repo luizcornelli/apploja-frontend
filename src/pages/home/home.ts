27fa09e44f5c671b460f1293d8af831ef4ac5603
Luiz Cornelli <luiz.cavalcanti@unidac.com.br>
2020-04-23 23:49:48 -0300

    Aproveitando usuário logado na tela inicial

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -43,4 +43,15 @@ export class HomePage {
     this.menu.swipeEnable(true);
   }
 
+  ionViewDidEnter(){
+
+    this.auth.refreshToken()
+      .subscribe(response => {
+        
+        this.auth.successfulLogin(response.headers.get('Authorization'))        
+        this.navCtrl.setRoot('CategoriasPage');
+      },
+      erro =>{});
+  }
+
 }
