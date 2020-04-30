e228d3f98adf9e4ffd2b2728c34ba3bb87efb3aa
Luiz Cornelli <luiz.cavalcanti@unidac.com.br>
2020-04-29 21:18:21 -0300

    Carregando imagens do produto

--- src/pages/produtos/produtos.ts
+++ src/pages/produtos/produtos.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import { ProdutoDTO } from '../../models/produto.dto';
 import { ProdutoService } from '../../services/domain/produto.service';
+import { API_CONFIG } from '../../config/api.config';
 
 @IonicPage()
 @Component({
@@ -24,8 +25,19 @@ export class ProdutosPage {
     this.produtoService.findByCategoria(categoria_id)
       .subscribe(response => {
         this.itens = response['content']; // Pegando o atributo content
+        this.loadImageUrls();
       },
       error => {}); 
   };
 
+  loadImageUrls() {
+    for(var i = 0; i < this.itens.length; i++){
+      let item = this.itens[i]
+      this.produtoService.getSmallImageFromBucket(item.id)
+        .subscribe(response => {
+          item.imageUrl = `${API_CONFIG.bucketBaseUrl}/prod${item.id}-small.jpg`;
+        }, 
+        error => {});
+    }
+  }
 }
