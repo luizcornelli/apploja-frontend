579b1d94f075d0819c98f3a98f816891d5203caf
Luiz Cornelli <luiz.cavalcanti@unidac.com.br>
2020-05-08 19:45:10 -0300

    Testando app no dispositivo

--- src/config/api.config.ts
+++ src/config/api.config.ts
@@ -1,5 +1,5 @@
 export const  API_CONFIG = {
     
-    baseUrl: "http://localhost:8080",
+    baseUrl: "https://apploja-ws.herokuapp.com",
     bucketBaseUrl: "https://apploja-ws.s3-sa-east-1.amazonaws.com"
 }
