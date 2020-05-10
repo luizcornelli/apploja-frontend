da168ec958d826722e703843c8f451038e396db0
Luiz Cornelli <luiz.cavalcanti@unidac.com.br>
2020-05-10 00:54:37 -0300

    Reinstalando dependencias e package-lock.json para corrigir problema de inputs

--- src/services/domain/pedido.service.ts
+++ src/services/domain/pedido.service.ts
@@ -1,7 +1,6 @@
 import { Injectable } from "@angular/core";
 import { HttpClient } from "@angular/common/http";
 import { API_CONFIG } from "../../config/api.config";
-import { StorageService } from "../storage.service";
 import { PedidoDTO } from "../../models/pedido.dto";
 
 @Injectable()
